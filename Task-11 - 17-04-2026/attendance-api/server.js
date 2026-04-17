e99830bda1227a538569c0dbd0173f4efaa399ae
Golu <vishal212121prajapati@gmail.com>
2026-04-17 16:28:17 +0530

    in this done project setup

---
+++ Task-11 - 17-04-2026/attendance-api/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
