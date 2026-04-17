df70b1597825a4e01b7c7408ad526a99eece6f4d
Golu <vishal212121prajapati@gmail.com>
2026-04-17 23:28:58 +0530

    done with phase-2 in this connected server with mysql database

--- Task-11 - 17-04-2026/attendance-api/server.js
+++ Task-11 - 17-04-2026/attendance-api/server.js
@@ -1,8 +1,9 @@
-const express = require("express");
 const dotenv = require("dotenv");
-const cors = require("cors");
-
 dotenv.config();
+
+const express = require("express");
+const cors = require("cors");
+const { connectDB } = require("./config/db");
 
 const app = express();
 
@@ -13,8 +14,10 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-const PORT = process.env.PORT || 5000;
+connectDB().then(() => {
+  const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
